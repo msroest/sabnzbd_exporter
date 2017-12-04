4d46bd176c85be83bdbafefd6b837012fb2cc93a
Mike Roest <mike@roe.st>
2017-12-03 21:33:20 -0700

    Update to use python logging instead of prints, switch to unbuffered python

--- sabnzbd_exporter.py
+++ sabnzbd_exporter.py
@@ -2,6 +2,7 @@ import os
 import requests
 import prometheus_client
 import threading
+import logging
 from prometheus_client import start_http_server
 from prometheus_client.core import GaugeMetricFamily, REGISTRY
 
@@ -12,8 +13,9 @@ if 'PORT' in os.environ:
 APIBASEURL = os.environ['SABNZBD_BASEURL']
 APIKEY = os.environ['SABNZBD_APIKEY']
 
-print("Starting sabnzbd_exporter on port: {}".format(PORT))
-print("Connecting to {}".format(APIBASEURL))
+logging.basicConfig(level=logging.INFO)
+logging.info("Starting sabnzbd_exporter on port: %d",PORT)
+logging.info("Connecting to %s",APIBASEURL)
 
 def getAPIUrl(mode):
     return '{}/api?output=json&apikey={}&mode={}'.format(APIBASEURL, APIKEY, mode)
@@ -44,8 +46,8 @@ class CustomCollector(object):
             yield GaugeMetricFamily('sabnzbd_queue_total_size_bytes','SABnzbd queue total size',value=float(queue_stats['mb'])*1024*1024)
             yield GaugeMetricFamily('sabnzbd_queue_remaining_seconds','SABnzbd estimated time remaining',value=get_sec(queue_stats['timeleft']))
         except Exception as inst:
-            print('Error getting stats')
-            print(inst)
+            logging.error('Error getting stats')
+            logging.error(inst)
 
 
 REGISTRY.register(CustomCollector())
